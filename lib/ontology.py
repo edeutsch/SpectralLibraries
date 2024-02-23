db97736f8f3edc55f068a06daf6b19ebbfb65b7f
edeutsch <edeutsch@systemsbiology.org>
2024-02-22 22:13:39 -0800

    minor tidying of header

--- lib/ontology.py
+++ lib/ontology.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 
-#from __future__ import print_function
-#import sys
-#def eprint(*args, **kwargs):
-#    print(*args, file=sys.stderr, **kwargs)
+import sys
+def eprint(*args, **kwargs): print(*args, file=sys.stderr, **kwargs)
 
 import logging
 import re
